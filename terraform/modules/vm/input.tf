407eb93e2ebb333baecfe57a32cb9e43cede6c88
Maksim Gorkii <gmaksim@beeline.com>
2022-12-20 11:41:39 -0500

    Add vm to modiles

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,15 @@
 
+# Resource Group
+variable resource_group {}
+variable location {}
+
+# Network
+variable subnet_id {}
+variable public_ip_address_id {}
+
+# VM
+variable vm_size {}
+variable "application_type" {}
+variable vm_admin_username {}
+variable vm_password {}
+variable "resource_type" {}
