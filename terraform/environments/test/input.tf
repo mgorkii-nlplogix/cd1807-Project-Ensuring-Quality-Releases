770f8cad0cb19fd5c57103e587cb4b0519cb128d
Maksim Gorkii <gmaksim@beeline.com>
2022-12-20 11:50:11 -0500

    Add vm to main input

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -14,3 +14,7 @@ variable virtual_network_name {}
 variable address_prefix_test {}
 variable address_space {}
 
+# VM
+variable "vm_size" {}
+variable "vm_admin_username" {}
+variable "vm_password" {}
