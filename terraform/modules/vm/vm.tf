d80fe67d3643378f93db1d1faa888cadbd72fc29
Maksim Gorkii <gmaksim@beeline.com>
2022-12-20 12:28:42 -0500

    Fix vm size

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -15,7 +15,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.resource_type}-${var.application_type}-vm"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  size                = "Standard_DS2_v2"
+  size                = "${var.vm_size}"
   admin_username      = "${var.vm_admin_username}"
   admin_password = "${var.vm_password}"
   network_interface_ids = [azurerm_network_interface.test.id]
